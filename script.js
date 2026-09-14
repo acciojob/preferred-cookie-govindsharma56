4e39de76c0a90762ebd2b0efd1b71cf18e85f680
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-14 19:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn=document.getElementById('btn-save')
+btn.addEventListener('click',()=>{
+let fontSize = document.getElementById("font-size").value;
+let fontColor = document.getElementById("font-color").value;
+   document.cookie = `fontSize=${fontSize}`;
+  document.cookie = `fontColor=${fontColor}`;
+})
