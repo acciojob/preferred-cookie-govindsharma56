e3a3dc108862d308a68df510a62edff1ca16892d
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-14 19:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 let btn=document.getElementById('btn-save')
 let cookies = document.cookie.split("; ");
 
-let fontSize = cookies.find((cookie) => cookie.startsWith("fontSize="));
-let fontColor = cookies.find((cookie) => cookie.startsWith("fontColor="));
+let fontSize = cookies.find((cookie) => cookie.startsWith("fontsize="));
+let fontColor = cookies.find((cookie) => cookie.startsWith("fontcolor="));
 
 fontSize = fontSize?.split("=")[1];
 fontColor = fontColor?.split("=")[1];
 
 if (fontSize) {
-  document.body.style.fontSize = fontSize + "px";
+  document.body.style.fontsize = fontSize + "px";
 }
 
 if (fontColor) {
