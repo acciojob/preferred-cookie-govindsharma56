77baa53a253074010a432a0f43556c95e80ef9ec
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-14 19:04:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //your JS code here. If required.
 let btn=document.getElementById('btn-save')
+let cookies = document.cookie.split("; ");
+
+let fontSize = cookies.find((cookie) => cookie.startsWith("fontSize="));
+let fontColor = cookies.find((cookie) => cookie.startsWith("fontColor="));
+
+fontSize = fontSize?.split("=")[1];
+fontColor = fontColor?.split("=")[1];
+
+if (fontSize) {
+  document.body.style.fontSize = fontSize + "px";
+}
+
+if (fontColor) {
+  document.body.style.color = fontColor;
+}
 btn.addEventListener('click',()=>{
 let fontSize = document.getElementById("font-size").value;
 let fontColor = document.getElementById("font-color").value;
